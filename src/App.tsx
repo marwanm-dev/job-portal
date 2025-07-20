84c2f0012ee08b8a57da4f8ae07bb2552b46d205
Marwan Mostafa <marwanm.dev@gmail.com>
2025-07-20 16:20:57 +0300

    feat: implement Overview section completely supporting smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar.tsx";
 import Hero from "./sections/Hero/Hero.tsx";
 import Features from "./sections/Features/Features.tsx";
+import Overview from "./sections/Overview/Overview.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col justify-evenly pt-30 pb-30 gap-30">
+      <div className="flex flex-col justify-evenly sm:pt-30 pt-15 sm:pb-30 pb-15 sm:gap-30 gap-15">
         <Hero />
         <Features />
+        <Overview />
       </div>
     </>
   );
